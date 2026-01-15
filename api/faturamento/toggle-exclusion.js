67db9658ce3ad32ed60d5553c6a5641c68a55441
Hyuri Branco <brasil.hyuri@gmail.com>
2026-01-15 15:52:30 -0300

    teste

--- api/faturamento/toggle-exclusion.js
+++ api/faturamento/toggle-exclusion.js
@@ -1,4 +1,4 @@
-import pool from '../db.js'; // Note os dois pontos (..) para voltar uma pasta
+import pool from '../../db.js'; // Note os dois pontos (..) para voltar uma pasta
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
