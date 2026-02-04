4458f9f2a0365f5ccdba56d428ba454d08e6a1b0
Hyuri Branco <brasil.hyuri@gmail.com>
2026-02-04 10:15:59 -0300

    fix: use shared db pool in weights API to resolve connection error

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 // api/index.js
+require('dotenv').config({ path: '.env.local' });
 const express = require('express');
 const app = express();
 const cors = require('cors'); // Adicionar CORS para requisições do frontend
