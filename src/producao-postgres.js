23493e1eab2bc692883dcc6177ff098f59b95ac6
Hyuri Branco <brasil.hyuri@gmail.com>
2026-02-18 13:36:27 -0300

    Atualização simples

--- src/producao-postgres.js
+++ src/producao-postgres.js
@@ -7,7 +7,7 @@ const pool = require('../lib/db');
 // Returns filtered productio records from the synced table
 router.get('/', async (req, res) => {
     try {
-        const { startDate, endDate, sector, search, limit = 10000 } = req.query;
+        const { startDate, endDate, sector, search, limit = 100000 } = req.query;
 
         let query = `
             SELECT 
