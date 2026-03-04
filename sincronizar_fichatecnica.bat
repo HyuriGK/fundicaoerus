dd1b32e1179975c4d9fe9021c5825df5cdddfa24
Hyuri Branco <brasil.hyuri@gmail.com>
2026-03-04 14:10:13 -0300

    Atualização comum

--- sincronizar_fichatecnica.bat
+++ sincronizar_fichatecnica.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Iniciando Sincronizacao de Fichas Tecnicas (Firebird para Postgres)...
-node scripts/sync-fichatecnica.js
+node scripts/reset-sync-fichatecnica.js
 echo.
 echo Sincronizacao Concluida!
 pause
