542d13ece4d99aa997b7687687c4d5d1996ecaef
Hyuri Branco <brasil.hyuri@gmail.com>
2026-03-25 10:20:19 -0300

    Atualização comum

---
+++ tmp/check_op_data.js
@@ -0,0 +1,22 @@
+const Firebird = require('node-firebird');
+const options = {
+    host: '10.1.1.100', port: 3050,
+    database: '/home/lm/LM-Sistemas/SIGE2.0/Dados/sige.fdb',
+    user: 'SYSDBA', password: 'masterkey'
+};
+
+Firebird.attach(options, (err, db) => {
+    if (err) { console.error(err); process.exit(1); }
+    
+    const sql = `
+        SELECT SETOR_PCS, STATUS_PCS, QUANTIDADE_PCS 
+        FROM PRODUCAO_SETOR 
+        WHERE CODIGO_PCS = 3235 AND EMPRESA_PCS = 10
+    `;
+    
+    db.query(sql, (err, result) => {
+        db.detach();
+        if (err) { console.error(err); process.exit(1); }
+        console.log(JSON.stringify(result, null, 2));
+    });
+});
