ef4ea409fb9024d067845fb38c8cae0f03aba4f3
Hyuri Branco <brasil.hyuri@gmail.com>
2026-04-13 15:55:31 -0300

    Atualização comum

---
+++ scratch/check_schema.js
@@ -0,0 +1,13 @@
+
+const pool = require('./lib/db');
+async function run() {
+    try {
+        const res = await pool.query(`SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'communications'`);
+        console.log(JSON.stringify(res.rows, null, 2));
+    } catch (e) {
+        console.error(e);
+    } finally {
+        process.exit();
+    }
+}
+run();
